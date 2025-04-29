b6c66e170c418efb32dbcfc18cb34f39550db1c7
Bhupendra Washishtha <bhupendrawashishtha@gmail.com>
2025-04-28 19:29:49 -0500

    Cleanup old node_modules and reset configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+    server: {
+      port: 5173, // optional, Vite default
+    },
+    build: {
+      outDir: 'dist',
+    },
+  };
+  
